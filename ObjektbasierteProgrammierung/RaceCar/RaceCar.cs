4370304828e8231003ca6c801158b0c423d2f6ca
Salome Spannring <salome.spannring@outlook.com>
2026-08-19 15:37:23 +0200

    Add Property 'CurrentSpeed'

--- ObjektbasierteProgrammierung/RaceCar/RaceCar.cs
+++ ObjektbasierteProgrammierung/RaceCar/RaceCar.cs
@@ -1,31 +1,47 @@
 ﻿namespace RaceCar;
 
 public class RaceCar
-    {
-        /* Instanzvariablen: 
-        Sämtliche neu erstellte Instanzen übernehmen folgende Felder.
-        Die Felder sind auf private damit sie von aussen nicht geändert werden können.*/ 
-        private int currentSpeed;
-        private int maxSpeed;
-        private int numberOfRounds;
-        
-        
-        // properties
-        public string BrandName { get; set; }
-        public string ModelName { get; set; }
-        public string ColorName { get; set; }
-        public string DriverName { get; set; }
-        public int StartingNumber { get; set; }
+{
+    /* Instanzvariablen:
+    Sämtliche neu erstellte Instanzen übernehmen folgende Felder.
+    Die Felder sind auf private damit sie von aussen nicht geändert werden können.*/
+    private int currentSpeed;
+    private int maxSpeed;
+    private int numberOfRounds;
 
-        public int MaxSpeed
+
+    // properties
+    public string BrandName { get; set; }
+    public string ModelName { get; set; }
+    public string ColorName { get; set; }
+    public string DriverName { get; set; }
+    public int StartingNumber { get; set; }
+
+    public int MaxSpeed
+    {
+        get { return this.maxSpeed; }
+        set
         {
-            get
+            if (value > 0)
             {
-                return this.maxSpeed;
-            }
-            set
-            {
-                maxSpeed = value;
+                this.maxSpeed = value;
             }
         }
     }
+
+    public int CurrentSpeed
+    {
+        get { return this.currentSpeed; }
+        set
+        {
+            if (value >= 0 && value >= this.MaxSpeed)
+            {
+                this.currentSpeed = value;
+            }
+            else
+            {
+                throw new 
+            }
+        }
+    }
+}
