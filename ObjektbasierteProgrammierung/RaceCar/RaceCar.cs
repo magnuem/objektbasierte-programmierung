34024bd9dcdab1648a0514d5ec44152d1981f3fe
Salome Spannring <salome.spannring@outlook.com>
2026-08-19 15:08:26 +0200

    Add RaceCar classes V.1

---
+++ ObjektbasierteProgrammierung/RaceCar/RaceCar.cs
@@ -0,0 +1,31 @@
+﻿namespace RaceCar;
+
+public class RaceCar
+    {
+        /* Instanzvariablen: 
+        Sämtliche neu erstellte Instanzen übernehmen folgende Felder.
+        Die Felder sind auf private damit sie von aussen nicht geändert werden können.*/ 
+        private int currentSpeed;
+        private int maxSpeed;
+        private int numberOfRounds;
+        
+        
+        // properties
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public string ColorName { get; set; }
+        public string DriverName { get; set; }
+        public int StartingNumber { get; set; }
+
+        public int MaxSpeed
+        {
+            get
+            {
+                return this.maxSpeed;
+            }
+            set
+            {
+                maxSpeed = value;
+            }
+        }
+    }
